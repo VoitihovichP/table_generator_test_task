7fa3ffa4cfd5b4048c0fd169ca3009aa11aac33b
Pavel Voitekhovich <pasha2003228@gmail.com>
2024-03-05 17:41:08 +0300

    feat: add some structure

---
+++ src/Screens/TablesScreen/TablesScreen.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import { Form } from '../../components/Form/Form.tsx';
+
+export const TablesScreen: FC = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./'),
+      Styles: path.resolve('./src/styles'),
+      Components: path.resolve('./src/components'),
+      ReduxActions: path.resolve('./src/store/actions'),
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    },
+  },
 });

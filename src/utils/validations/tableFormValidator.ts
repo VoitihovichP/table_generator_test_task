c9b12b315a62eead0a76abd7546cd486cf78bdcb
Pavel Voitekhovich <pasha2003228@gmail.com>
2024-03-18 17:27:07 +0300

    feat: add custom validations

--- src/utils/validations/tableFormValidator.ts
+++ src/utils/validations/tableFormValidator.ts
@@ -1,8 +1,16 @@
 import * as yup from 'yup';
+import { MaxStringLengthValidator } from 'Utils/validations/CommonValidators.ts';
 
 export const createRowValidator = yup.object().shape({
-  name: yup.string().required('Required field'),
-  surname: yup.string().required('Required field'),
-  age: yup.string().required('Required field'),
+  name: MaxStringLengthValidator(100).required('Required field'),
+  surname: MaxStringLengthValidator(100).required('Required field'),
+  age: yup
+    .string()
+    .required('Required field')
+    .test(
+      'greaterThanZero',
+      'Must be greater than 0',
+      (value) => Number(value) > 0,
+    ),
   city: yup.string().required('Required field'),
 });
